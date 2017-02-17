3cef4ee52ec1f6f1fe90f50a1b4f7e58127f94da
feng <eric.zhang@okkuaixiu.com>
2017-02-17 14:51:21 +0800

    修改页面，添加imageload引用

---
+++ app/src/main/java/com/umeng/test/Util/ImageLoad.java
@@ -0,0 +1,147 @@
+package com.umeng.test.Util;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiskCache;
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.nostra13.universalimageloader.utils.StorageUtils;
+import com.umeng.test.R;
+
+import java.io.File;
+
+/**
+ * Created by Administrator on 2016/11/29.
+ */
+
+public class ImageLoad {
+
+    private Context context;
+    private ImageLoaderConfiguration config;
+    private ImageLoader loader = ImageLoader.getInstance();
+    private DisplayImageOptions options;
+
+    public ImageLoad(Context context) {
+        this.context = context;
+        initImageLoader(this.context);
+    }
+
+    /**
+     * 加载原图
+     *
+     * @param iv
+     * @param url
+     */
+    public void loadImage(final ImageView iv, final String url) {
+        try {
+
+            loader.displayImage(url, iv, options);
+            // loader.loadImage(url, options, new SimpleImageLoadingListener() {
+            //
+            // 	@Override
+            // 	public void onLoadingComplete(String imageUri, View view,
+            // 			Bitmap loadedImage) {
+            // 		super.onLoadingComplete(imageUri, view, loadedImage);
+            // 		if (url.equals(iv.getTag())) {
+            // 			if (iv != null) {
+            // 				iv.setImageBitmap(loadedImage);
+            // 			}
+            // 		}
+            // 	}
+            // });
+        } catch (Exception e) {
+            LogUtil.e(getClass(), "loadImage()", e);
+        }
+    }
+
+    /**
+     * 加载指定大小图片
+     *
+     * @param iv
+     * @param url
+     * @param width
+     * @param height
+     */
+    public void loadImage(final ImageView iv, final String url, int width,
+                          int height) {
+        try {
+            // iv.setTag(url);
+            // ImageSize imageSize = new ImageSize(width, height);
+            // loader.displayImage(url, iv, options);
+            // loader.loadImage(url, imageSize, options,
+            // new SimpleImageLoadingListener() {
+            //
+            // @Override
+            // public void onLoadingComplete(String imageUri,
+            // View view, Bitmap loadedImage) {
+            // super.onLoadingComplete(imageUri, view, loadedImage);
+            // if (url.equals(iv.getTag())) {
+            // iv.setImageBitmap(loadedImage);
+            // }
+            // }
+            // });
+            loader.displayImage(url, iv, options);
+        } catch (Exception e) {
+            LogUtil.e(
+                    getClass(),
+                    "loadImage(final ImageView iv, final String url, int width,int height)",
+                    e);
+        }
+    }
+
+    public void clearCache() {
+        try {
+
+            if (loader != null) {
+                // loader.clearDiskCache();
+                loader.clearMemoryCache();
+            }
+
+        } catch (Exception e) {
+            LogUtil.e(getClass(), "clearCache()", e);
+        }
+    }
+
+    /**
+     * 初始化ImageLoader数据
+     */
+    private void initImageLoader(Context context) {
+        try {
+
+            options = new DisplayImageOptions.Builder()
+                    .bitmapConfig(Bitmap.Config.RGB_565)
+                    .imageScaleType(ImageScaleType.EXACTLY)
+                    .showImageOnLoading(R.drawable.icon_default)
+                    .showImageForEmptyUri(R.drawable.icon_default)
+                    .showImageOnFail(R.drawable.icon_default)
+                    .cacheInMemory(true).cacheOnDisk(true).build();
+            File cacheDir = StorageUtils.getCacheDirectory(context);
+            // 设置缓存路径和缓存文件的数量，超过数量后，old将被删除
+            config = new ImageLoaderConfiguration.Builder(context)
+                    .threadPoolSize(5)
+                    // 设定线程池的大小
+                    .threadPriority(Thread.NORM_PRIORITY - 1)
+                    .denyCacheImageMultipleSizesInMemory()
+                    .diskCacheSize(50 * 1024 * 1024).diskCacheFileCount(100)
+                    .diskCache(new UnlimitedDiskCache(cacheDir))
+                    .defaultDisplayImageOptions(options)
+                    .memoryCache(new WeakMemoryCache())
+                    .memoryCacheSize(2 * 1024 * 1024)
+                    .diskCacheFileNameGenerator(new Md5FileNameGenerator())
+                    .tasksProcessingOrder(QueueProcessingType.LIFO).build();
+            ImageLoader.getInstance().init(config);
+
+        } catch (Exception e) {
+            LogUtil.e(getClass(), "initImageLoader(Context context)", e);
+        }
+    }
+
+}
+
---
+++ app/src/main/java/com/umeng/test/adapter/GridviewAdapter.java
@@ -0,0 +1,72 @@
+package com.umeng.test.adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.umeng.test.R;
+import com.umeng.test.Util.ImageLoad;
+
+import java.util.ArrayList;
+
+public class GridviewAdapter extends BaseAdapter {
+	private Context mcontext;
+	private ArrayList<String> list_img;
+	private LayoutInflater inflater;
+	private ImageLoad imageLoad;
+	private String img_url ;
+
+	public GridviewAdapter(Context context, ArrayList<String> list_img_url) {
+		this.mcontext = context;
+		this.list_img = list_img_url;
+		imageLoad = new ImageLoad(mcontext);
+		inflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return list_img.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return list_img.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return position;
+	}
+
+	@SuppressLint("InflateParams")
+	@Override
+	public View getView(final int position, View convertView, ViewGroup parent) {
+		MyHolder holder = null;
+		if (convertView == null) {
+			convertView = inflater.inflate(R.layout.gridview_item, null);
+			holder = new MyHolder();
+			holder.iv_img = (ImageView) convertView.findViewById(R.id.iv_img);
+			convertView.setTag(holder);
+		} else {
+			holder = (MyHolder) convertView.getTag();
+		}
+		img_url = list_img.get(position);
+		// if (img_url.contains(HttpUrlNew.HTTP_DOMAIN)) {
+		//	img_url = "http://" + img_url;
+		// }
+		imageLoad.loadImage(holder.iv_img, img_url);
+		return convertView;
+	}
+
+	class MyHolder {
+		ImageView iv_img;// 箭头
+	}
+}
